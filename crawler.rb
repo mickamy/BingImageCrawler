359cc92d6f372e50e55afeddb550d0a8746271c1
Tetsuro Mikami <t.mikami1992@gmail.com>
2016-07-26 22:02:20 +0900

    Forgot to add stage

--- crawler.rb
+++ crawler.rb
@@ -2,10 +2,7 @@ require 'searchbing'
 require 'yaml'
 
 config = YAML.load_file('config.yml')
-puts config[:api_key]
-puts config[:request_count]
-puts config[:query]
 
-# bing = Bing.new(config[:api_key], config[:request_count], 'Image')
-# results = bing.search(config[:query])
-# puts results
+bing = Bing.new(config[:api_key], config[:request_count], 'Image')
+results = bing.search(config[:query])
+puts results
