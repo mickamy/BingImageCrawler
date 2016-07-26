e6d292d4c3c4e872a7c7c9cdc7e4acb2f22d9390
Tetsuro Mikami <t.mikami1992@gmail.com>
2016-07-27 00:42:18 +0900

    Make open-uri redirect enabled

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
-source "https://rubygems.org"
-ruby "2.2.4"
+source 'https://rubygems.org'
+ruby '2.2.4'
 
 gem 'searchbing', '~> 0.2.4'
+gem 'open_uri_redirections'
--- crawler.rb
+++ crawler.rb
@@ -1,22 +1,27 @@
 require 'searchbing'
 require 'yaml'
 require 'open-uri'
+require 'open_uri_redirections'
+require 'FileUtils'
 
-def save_image(url)
+def save_image(url, dir_name)
 	filename = File.basename(url)
-	open('images/' + filename.to_s, 'wb') do |file|
-    open(url) do |data|
+	open(dir_name + '/' + filename.to_s, 'wb') do |file|
+    open(url, allow_redirections: :all) do |data|
       file.write(data.read)
     end
   end
 end
 
 config = YAML.load_file('config.yml')
+dir_name = config[:dir_name]
+
+FileUtils.mkdir_p(dir_name) unless FileTest.exist?(dir_name)
 
 bing = Bing.new(config[:api_key], config[:request_count], 'Image')
 results = bing.search('clothing laundry tag')
 
 results[0][:Image].each do |result|
   puts result[:MediaUrl]
-  save_image(result[:MediaUrl])
+  save_image(result[:MediaUrl], dir_name)
 end
