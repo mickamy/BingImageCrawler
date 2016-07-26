f243309e629226f07b6d738cbb829cfa1eee3762
Tetsuro Mikami <t.mikami1992@gmail.com>
2016-07-27 00:17:34 +0900

    Save image

--- crawler.rb
+++ crawler.rb
@@ -1,8 +1,22 @@
 require 'searchbing'
 require 'yaml'
+require 'open-uri'
+
+def save_image(url)
+	filename = File.basename(url)
+	open('images/' + filename.to_s, 'wb') do |file|
+    open(url) do |data|
+      file.write(data.read)
+    end
+  end
+end
 
 config = YAML.load_file('config.yml')
 
 bing = Bing.new(config[:api_key], config[:request_count], 'Image')
 results = bing.search('clothing laundry tag')
-puts results
+
+results[0][:Image].each do |result|
+  puts result[:MediaUrl]
+  save_image(result[:MediaUrl])
+end
