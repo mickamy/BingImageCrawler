0a3aab01fca32b86dc357d91c3eb08b2a82c98f7
tetsuro.mikami <t.mikami1992@gmail.com>
2016-07-27 09:37:25 +0900

    Bug fix

--- crawler.rb
+++ crawler.rb
@@ -2,12 +2,11 @@ require 'searchbing'
 require 'yaml'
 require 'open-uri'
 require 'open_uri_redirections'
-require 'FileUtils'
+require 'fileutils'
 require 'date'
 
-saved_image_count = 0
-
 def save_image(url, dir_name)
+  success = false
 	filename = File.basename(url)
   path = dir_name + '/' + filename.to_s
 	open(path, 'wb') do |file|
@@ -15,13 +14,14 @@ def save_image(url, dir_name)
       open(url, allow_redirections: :all) do |data|
         file.write(data.read)
         puts "Saved #{url}"
-        save_image_count += 1
+        success = true
       end
     rescue OpenURI::HTTPError => e
       puts "Couldn't get a image #{url} cause = #{e}"
       FileUtils.rm(file.path)
     end
   end
+  success
 end
 
 config = YAML.load_file('config.yml')
@@ -34,15 +34,19 @@ dir_name = "output_#{date}"
 FileUtils.mkdir_p(dir_name) unless FileTest.exist?(dir_name)
 bing = Bing.new(config[:api_key], request_limit, 'Image')
 
+saved_count = 0
+
 request_times.times do |count|
   offset = count * request_limit
   results = bing.search('clothing laundry tag', offset)
 
   results[0][:Image].each do |result|
-    save_image(result[:MediaUrl], dir_name)
+    if save_image(result[:MediaUrl], dir_name)
+      saved_count += 1
+    end
   end
 end
 
 puts '-' * 20
 puts 'Finish!'
-puts "Saved #{saved_image_count}} images!"
+puts "Saved #{saved_count}} images!"
